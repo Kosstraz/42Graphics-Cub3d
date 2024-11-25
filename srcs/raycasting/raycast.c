b676a0346859e6edb61dfdd2178a8107b5673bad
cow <cow@mail.com>
2024-11-25 23:45:58 +0100

    abandon

--- srcs/raycasting/raycast.c
+++ srcs/raycasting/raycast.c
@@ -12,40 +12,12 @@
 
 #include "cub3d.h"
 
-float	get_x_wall(t_core *core)
-{
-	float	m;
-	int		x;
-	int		sign_x;
-
-	if (core->player.view.dx = 0)
-		return ();
-	else if (core->player.view.dx > 0)
-		sign_x = 1;
-	else
-		sign_x = -1;
-	m = core->player.view.dy / core->player.view.dx * sign_x;
-	if (core->player.view.dy > 0)
-		x = (int) core->player.position.x;
-	else if (core->player.view.dy == 0)
-		
-
-}
-
 void	cast_ray(t_core *core, int i)
 {
-	//float angle;
-
-	//angle = FOV / NB_RAYS * i;
-	// int	x1;
-	// int	y1;
-	// int	x2;
-	// int y2;
-	// float	x_wall;
-	// float	y_wall;
-
-	// x1 = core->player.position.x;
-	// y1 = core->player.position.y;
+	float pa;
+	
+	pa = atan2(core->player.view.dx, core->player.view.dy);
+	
 	
 
 }
