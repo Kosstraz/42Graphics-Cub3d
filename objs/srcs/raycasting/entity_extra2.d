6417eef2c0cc88fc185288f4559dfc6eb91d78b7
Yann Manchon <ymanchon@bess-f2r2s7.clusters.42paris.fr>
2025-02-12 14:47:10 +0100

    finish

---
+++ objs/srcs/raycasting/entity_extra2.d
@@ -0,0 +1,3 @@
+objs/./srcs/raycasting/entity_extra2.obj: srcs/raycasting/entity_extra2.c \
+  includes/cub3d.h MLX42-2.4.1/include/MLX42/MLX42.h libft/libft.h \
+  includes/platform.h includes/defs.h includes/enums.h
---
+++ objs/srcs/utils_colors2.d
@@ -0,0 +1,3 @@
+objs/./srcs/utils_colors2.obj: srcs/utils_colors2.c includes/cub3d.h \
+  MLX42-2.4.1/include/MLX42/MLX42.h libft/libft.h includes/platform.h \
+  includes/defs.h includes/enums.h
